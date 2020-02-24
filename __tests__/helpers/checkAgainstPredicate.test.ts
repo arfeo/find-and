d822c5809d1529eccde9b50e23529ec40a61bb96
Leonid Belikov <mail@arfeo.net>
2020-02-24 22:03:07 +0300

    test update

--- __tests__/helpers/checkAgainstPredicate.test.ts
+++ __tests__/helpers/checkAgainstPredicate.test.ts
@@ -1,11 +1,24 @@
 import { checkAgainstPredicate } from '../../src/helpers';
 
+interface HashMap {
+  [key: string]: any;
+}
+
+const data: HashMap = {
+  id: 1,
+  name: 'One',
+};
+
 describe('checkAgainstPredicate function', (): void => {
-  test('should return true if all props of the given predicate exist and are equal to props of the given source item', (): void => {
-    expect(checkAgainstPredicate({ id: 1, name: 'One' }, { id: 1, name: 'One' })).toBe(true);
+  test('should return true if all props of the given predicate exist and are equal to props of the given source item # 1', (): void => {
+    expect(checkAgainstPredicate(data, { id: 1 })).toBe(true);
+  });
+
+  test('should return true if all props of the given predicate exist and are equal to props of the given source item # 2', (): void => {
+    expect(checkAgainstPredicate(data, { id: 1, name: 'One' })).toBe(true);
   });
 
   test('should return false if not all props of the given predicate exist and are equal to props of the given source item', (): void => {
-    expect(checkAgainstPredicate({ id: 1, name: 'One' }, { id: 1, name: 'Two' })).toBe(false);
+    expect(checkAgainstPredicate(data, { id: 1, name: 'Two' })).toBe(false);
   });
 });
